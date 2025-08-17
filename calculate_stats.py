d1b4ac34986c89987a62d6d639c5bb8e94765c97
lombardi-g <gabriellomba@gmail.com>
2025-08-17 16:00:07 -0300

    total plate appearances

--- calculate_stats.py
+++ calculate_stats.py
@@ -3,3 +3,12 @@ import pandas as pd
 data = pd.read_csv("appearances.csv", sep=';')
 
 players = data["Name"].unique()
+
+plate_appearances = {}
+
+for player in players:
+    appeared = data.loc[data["Name"]==player,"Name"].count()
+    # print(type(appeared))
+    plate_appearances[player] = int(appeared)
+
+print(plate_appearances)
