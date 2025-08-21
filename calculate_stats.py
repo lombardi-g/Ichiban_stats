4f91cb40113edab724d245ec30f13be8916e9a2f
lombardi-g <gabriellomba@gmail.com>
2025-08-21 08:26:53 -0300

    removing previous code

--- calculate_stats.py
+++ calculate_stats.py
@@ -8,20 +8,3 @@ count_towards_atbats = ["1b", "2b", "3b", "HR", "K", "ROE", "RFC", "GO", "FO"]
 
 plate_appearances = data.groupby("Name").size()
 atbats = data[data["Appearance"].isin(count_towards_atbats)].groupby("Name").size()
-
-# for player in players:
-#     appeared = data.loc[data["Name"]==player,"Name"].count()
-#     plate_appearances[player] = int(appeared)
-#     turns_at_bat = data.loc[
-#         ((data["Appearance"] == "1b") |
-#         (data["Appearance"] == "2b") |
-#         (data["Appearance"] == "3b") |
-#         (data["Appearance"] == "HR") |
-#         (data["Appearance"] == "K") |
-#         (data["Appearance"] == "ROE") |
-#         (data["Appearance"] == "RFC") |
-#         (data["Appearance"] == "GO") |
-#         (data["Appearance"] == "FO")) &
-#         (data["Name"] == player) 
-#         ,"Appearance"].count()
-#     at_bats[player] = int(turns_at_bat)
