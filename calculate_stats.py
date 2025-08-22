8273c0f6b05ae2808e19fa4f244c39902cbde432
lombardi-g <gabriellomba@gmail.com>
2025-08-22 08:21:01 -0300

    lists for hits and obp

--- calculate_stats.py
+++ calculate_stats.py
@@ -5,6 +5,8 @@ data = pd.read_csv("appearances.csv", sep=';')
 players = data["Name"].unique()
 
 count_towards_atbats = ["1b", "2b", "3b", "HR", "K", "ROE", "RFC", "GO", "FO"]
+count_as_hits = ["1b", "2b", "3b", "HR"]
+on_base = ["1b", "2b", "3b", "HR", "BB", "HBP"]
 
 plate_appearances = data.groupby("Name").size()
 atbats = data[data["Appearance"].isin(count_towards_atbats)].groupby("Name").size()
