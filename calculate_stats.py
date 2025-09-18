25781962d5e9ea56c3c7563b304344bbfce66667
lombardi-g <gabriellomba@gmail.com>
2025-09-18 08:11:12 -0300

    adding K stat

--- calculate_stats.py
+++ calculate_stats.py
@@ -13,6 +13,8 @@ atbats = data[data["Appearance"].isin(count_towards_atbats)].groupby("Name").siz
 hits = data[data["Appearance"].isin(count_as_hits)].groupby("Name").size()
 got_on_base = data[data["Appearance"].isin(on_base)].groupby("Name").size()
 
+strikeouts = data[data["Appearance"]=="K"].groupby("Name").size()
+
 average = (hits/atbats).round(3)
 on_base_percentage = (got_on_base/plate_appearances).round(3)
 
